07bf016292e71ea528f2b284b2b07a1fa4d3b556
zym0tic <lukerozon@gmail.com>
2023-02-06 18:44:44 -0500

    ListHeader/ buttons and style

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function ListHeader({ listName }) {
+
+const signOut = () => {
+  console.log('sign out')
+}
+
   return (
     <div className='list-header'>
       <h1>{listName}</h1>
+      <div className='button-container'>
+        <button className='create'>Add New</button>
+        <button className='signout' onClick={signOut}>Sign Out</button>
+      </div>
       </div>
   )
 }
