f7e90fe20ffe965158888429ba5f72b9e67f5a5d
zym0tic <lukerozon@gmail.com>
2023-02-16 17:03:18 -0500

    add tickIcon, ProgressBar, and buttons to List item

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import TickIcon from './TickIcon'
+import ProgressBar from './ProgressBar'
 
-function ListItem({task}) {
+function ListItem({ task }) {
   return (
-    <div className='list-item'>
-      <p>{task.title}</p>
+    <div className="list-item">
+      <div className="info-container">
+        <TickIcon/>
+        <p>{task.title}</p>
+        <ProgressBar />
+      </div>
+      <div className="button-container">
+        <button className="edit">EDIT</button>
+        <button className="delete">DELETE</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ListItem
+export default ListItem;
