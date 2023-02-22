c92c25e05aa6ac9f3e4e7394cfcc207f96233fd7
zym0tic <lukerozon@gmail.com>
2023-02-22 15:38:25 -0500

    saving progress on auth component

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 function Auth() {
+  const isLogin = false
   return (
-    <div>Auth</div>
+    <div className='auth-container'>
+      <div className='auth-container-box'>
+      <form>
+        <h2>{isLogin ? 'Please log in' : 'Please sign up'}</h2>
+        <input type="email" placeholder='email'/>
+        <input type="password" placeholder='password'/>
+        {isLogin && <input type="password" placeholder='confirm password'/>}
+        <input type="submit" className='create'/>
+      </form>
+      </div>
+    </div>
   )
 }
 
