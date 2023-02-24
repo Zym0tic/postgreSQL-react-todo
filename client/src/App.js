e4b4ee8fcebdd3bb09c3fdb320258e4103099711
zym0tic <lukerozon@gmail.com>
2023-02-23 23:21:21 -0500

    display auth modal when not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,49 @@
-import { useEffect, useState } from 'react'
-import ListHeader from './components/ListHeader'
-import ListItem from'./components/ListItem'
-
+import { useEffect, useState } from "react";
+import ListHeader from "./components/ListHeader";
+import Auth from "./components/Auth";
+import ListItem from "./components/ListItem";
 
 const App = () => {
-    const userEmail = 'luker@test.com'
-    const [tasks, setTasks] = useState(null)
+  const userEmail = "luker@test.com";
+  const [tasks, setTasks] = useState(null);
 
-const getData = async () => {
-    try{
-        const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
-        const json = await response.json()
-        setTasks(json)
+  const AuthToken = false;
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`
+      );
+      const json = await response.json();
+      setTasks(json);
     } catch (err) {
-        console.error(err)
-        console.log(process.env.REACT_APP_SERVERURL)
+      console.error(err);
+      console.log(process.env.REACT_APP_SERVERURL);
     }
-}
+  };
 
-useEffect(() => getData, [])
+  useEffect(() => getData, []);
 
-    console.log(tasks)
+  console.log(tasks);
 
-    // sort by date
-    const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
+  // sort by date
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
-    return (
-        <div className='app'>
-            <ListHeader listName={'Holiday tick List'} getData={getData}/>
-            {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
-        </div>
-    )
-}
+  return (
+    <div className="app">
+        {!AuthToken && <Auth/>}
+      {AuthToken && 
+        <>
+          <ListHeader listName={"Holiday tick List"} getData={getData} />
+          {sortedTasks?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}
+        </>
+      }
+    </div>
+  );
+};
 
 export default App;
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Auth() {
-  const [isLogin, setIsLogin] = useState(true)
-  const [error, setError] = useState(null)
+  const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState(null);
 
   const viewLogin = (status) => {
-    setError(null)
-    setIsLogin(true)
-  }
-
+    setError(null);
+    setIsLogin(true);
+  };
 
   return (
-    <div className='auth-container'>
-      <div className='auth-container-box'>
-      <form>
-        <h2>{isLogin ? 'Please log in' : 'Please sign up'}</h2>
-        <input type="email" placeholder='email'/>
-        <input type="password" placeholder='password'/>
-        {isLogin && <input type="password" placeholder='confirm password'/>}
-        <input type="submit" className='create'/>
-        {error && <p>{error}</p>}
-      </form>
-      <div className='auth-options'>
-        <button onClick={() => viewLogin(false)}>Sign Up</button>
-        <button onClick={() => viewLogin(true)}>Log In</button>
-      </div>
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogin ? "Please log in" : "Please sign up"}</h2>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          {isLogin && <input type="password" placeholder="confirm password" />}
+          <input type="submit" className="create" />
+          {error && <p>{error}</p>}
+        </form>
+        <div className="auth-options">
+          <button
+            onClick={() => viewLogin(false)}
+            style={{
+              backgroundColor: !isLogin
+                ? "rgb(255, 255, 255)"
+                : "rgb(188, 188, 188)",
+            }}
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => viewLogin(true)}
+            style={{
+              backgroundColor: isLogin
+                ? "rgb(255, 255, 255)"
+                : "rgb(188, 188, 188)",
+            }}
+          >
+            Log In
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
