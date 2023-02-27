e152eecc03c76d2837080c2ac5586ba472840bc3
zym0tic <lukerozon@gmail.com>
2023-02-27 00:39:50 -0500

    change listtitle

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ const App = () => {
         {!AuthToken && <Auth/>}
       {AuthToken && 
         <>
-          <ListHeader listName={"Holiday tick List"} getData={getData} />
+          <ListHeader listName={"To-do Checklist!"} getData={getData} />
           <p className="user-email">Welcome back {userEmail}!</p>
           {sortedTasks?.map((task) => (
             <ListItem key={task.id} task={task} getData={getData} />
